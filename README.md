5be2965c5236be03f220fa45fe36dc2fa893c1a6
xujiaji <jiajixu@qq.com>
2018-11-13 18:40:42 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# 一个项目如何编译多个不同签名、包名、资源等，的apk？
+文章地址：[https://blog.xujiaji.com/post/android-project-one-for-more](https://blog.xujiaji.com/post/android-project-one-for-more)
+
+该项目为该文章Demo
+
+## 运行时选择渠道
+<img  src="https://raw.githubusercontent.com/xujiaji/xujiaji.github.io/pictures/blog/one-for-more/20181113182242.png" width="22%" height="auto">
+
+## 选择不同渠道将运行不同的apk
+<img  src="https://raw.githubusercontent.com/xujiaji/xujiaji.github.io/pictures/blog/one-for-more/Screenshot_2018-11-13-18-20-13-184_com.miui.home.png" width="22%" height="auto">
+
+## 这些apk，都有不同的资源配置
+<div >
+<img  src="https://raw.githubusercontent.com/xujiaji/xujiaji.github.io/pictures/blog/one-for-more/20181113182506.png" width="33%" height="auto">
+<img style="margin-left:0px;" src="https://raw.githubusercontent.com/xujiaji/xujiaji.github.io/pictures/blog/one-for-more/20181113182534.png" width="33%" >
+<img style="margin-left:0px;" src="https://raw.githubusercontent.com/xujiaji/xujiaji.github.io/pictures/blog/one-for-more/20181113182556.png" width="33%" >
+</div>
---
+++ app/src/main/java/com/xujiaji/oneforallapk/MainActivity.java
@@ -0,0 +1,47 @@
+package com.xujiaji.oneforallapk;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTvVersion;
+    private TextView mTvUseJava;
+    private ImageView mImgGirl;
+    private ImageView mImgAssetsGirl;
+    private ImageView mImgNet;
+    private TextView mTvQQId;
+    private TextView mTvWxId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTvVersion = findViewById(R.id.tvVersion);
+        mTvUseJava = findViewById(R.id.tvUseJava);
+        mImgGirl = findViewById(R.id.imgGirl);
+        mImgAssetsGirl = findViewById(R.id.imgAssetsGirl);
+        mImgNet = findViewById(R.id.imgNet);
+        mTvQQId = findViewById(R.id.tvQQId);
+        mTvWxId = findViewById(R.id.tvWxId);
+
+        mTvVersion.setText(getString(R.string.version_) + BuildConfig.VERSION_NAME);
+        mTvUseJava.setText(getString(R.string.use_java) + Config.CONFIG);
+
+        mImgGirl.setImageResource(R.mipmap.girl);
+        Glide.with(this)
+                .load("file:///android_asset/img.jpg")
+                .into(mImgAssetsGirl);
+
+        Glide.with(this)
+                .load(BuildConfig.LOGOURL)
+                .into(mImgNet);
+
+        mTvQQId.setText(getString(R.string.qq_id) + BuildConfig.QQ_ID);
+        mTvWxId.setText(getString(R.string.wx_id) + BuildConfig.WX_ID);
+    }
+}
